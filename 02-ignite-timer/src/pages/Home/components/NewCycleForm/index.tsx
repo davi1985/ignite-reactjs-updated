d22b49a9e38164a20d104fe31dd91bf9d4dfb0cb
Davi Silva <davisilvaphoto@gmail.com>
2022-10-04 14:50:16 -0300

    history for cycles

--- 02-ignite-timer/src/pages/Home/components/NewCycleForm/index.tsx
+++ 02-ignite-timer/src/pages/Home/components/NewCycleForm/index.tsx
@@ -20,9 +20,10 @@ export function NewCycleForm() {
 
       <datalist id="task-suggestions">
         <option value="Projeto 1" />
-        <option value="Projeto 1" />
-        <option value="Projeto 1" />
-        <option value="Projeto 1" />
+        <option value="Projeto 2" />
+        <option value="Projeto 3" />
+        <option value="Projeto 4" />
+        <option value="Projeto 5" />
       </datalist>
 
       <label htmlFor="">durante</label>
